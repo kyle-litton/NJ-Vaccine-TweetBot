3f864cef204ea5796e652113841a6a95475bda62
James Kyle Litton <99kylel@gmail.com>
2021-03-09 17:34:32 -0500

    fixed json keys

--- HackensackBot.py
+++ HackensackBot.py
@@ -3,6 +3,11 @@ import time
 import tweepy
 from bs4 import BeautifulSoup
 
+import json
+key_file = 'keys.json'
+with open(key_file) as f:
+    keys = json.load(f)
+
 url = "https://mychart.hmhn.org/MyChart/SignupAndSchedule/EmbeddedSchedule?dept=1110101656,1110301124&vt=112916"
 
 # Headers to mimic a browser visit
