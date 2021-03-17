8b6be9aad08a73c3eba5229074e8b26ffa759f81
Gianna Aprile <giannaaprile99@gmail.com>
2021-03-17 19:31:41 -0400

    started VNACJ bot

--- VNACJBot.py
+++ VNACJBot.py
@@ -5,6 +5,7 @@ from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from playsound import playsound
 import tweepy
+import random
 import time
 import json
 
@@ -16,33 +17,47 @@ auth = tweepy.OAuthHandler(keys["consumer_key"], keys["consumer_secret"])
 auth.set_access_token(keys["access_token"], keys["access_token_secret"])
 api = tweepy.API(auth)
 
-url = "https://curogram.com/registrations/5fe2fe643b4a850044b0b3b1/blocked"
+url = "https://curogram.com/registrations/5fe2fe643b4a850044b0b3b1"
 
 chrome_options = Options()
 #chrome_options.add_argument('--headless')
 driver = webdriver.Chrome(options=chrome_options,executable_path='Drivers/chromedriver')
+
+
 while True:
 
-    driver.get(url)
-    time.sleep(2)
-    mainFrame = WebDriverWait(driver, 2).until(
-        EC.presence_of_element_located((By.XPATH, '/html/body/curo-root/curo-intel-widget-layout/div/curo-registrations/curo-registration-blocked'))
-    )
-    
-    # TODO fix this try/except to be the correct way once we see open VNACJ appts
-
-    try: # Look for error page, if found continue refreshing
-        element = WebDriverWait(driver, 2).until(
-        EC.presence_of_element_located((By.XPATH, '/html/body/curo-root/curo-intel-widget-layout/div/curo-registrations/curo-registration-blocked/section/h1'))
-    )
+    try:
+        driver.delete_all_cookies()
+        driver.get(url)
+        time.sleep(random.uniform(1.4,2.3))
+    except:
+        time.sleep(random.uniform(120,150))
+        print("URL Did not load.")
         continue
 
-    except: # Error page didn't load --> there may be appointments
+
+
+    # TODO may not need to do this: Check if the curo-root tag is there, avoids the white loading screen
+    try:
+        root = WebDriverWait(driver, random.uniform(1.7,2.2)).until(
+            EC.presence_of_element_located((By.XPATH, '/html/body/curo-root'))
+        )
+    except:
+        time.sleep(random.uniform(3.2,4.6))
+        print("No root tag.")
+        continue
+
+    
+
+    try: # Look for Step 1/13 Introduction page, if not found, continue refreshing
+        element = WebDriverWait(driver, 2).until(
+            EC.presence_of_element_located((By.XPATH, '/html/body/curo-root/curo-intel-widget-layout/div/curo-registrations/curo-registration-steps'))
+        )
 
         playsound('Beep.m4a')
         driver.get_screenshot_as_file("Screenshots/VNACJcapture.png")
 
-        status = "VNACJ: Portal is open at this link https://curogram.com/registrations/5fe2fe643b4a850044b0b3b1/blocked"
+        status = "VNACJ: Portal is open at this link https://curogram.com/registrations/5fe2fe643b4a850044b0b3b1"
         imagePath = "Screenshots/VNACJcapture.png"
 
         print(element.get_attribute("class"))
@@ -50,4 +65,31 @@ while True:
         # TODO uncomment once we have it fully working
         # api.update_with_media(imagePath, status)
         break
+        
+    except: # Portal not open, no appointmetns
+        print("portnal not open")
+        continue
+
+
+    #TODO try: # Portal is open, click the continue button on the introduction page
+
+        # TODO click continue button here
+    
+    #except: # Could not click continue button
+        #continue
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
