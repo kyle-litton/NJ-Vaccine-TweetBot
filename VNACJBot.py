41de7baf556d225420fa3e856d56421318da2237
Gianna Aprile <giannaaprile99@gmail.com>
2021-03-22 20:43:01 -0400

    added a sleep after clicking initial location

--- VNACJBot.py
+++ VNACJBot.py
@@ -4,10 +4,44 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from playsound import playsound
+from selenium.webdriver.support.color import Color
 import tweepy
 import random
 import time
 import json
+
+def countAppointments(availability_container):
+    # TODO get the correct green color vvvvvvv and update this placeholder
+
+    GREY_COLOR = Color.from_string('#F4F6F7')
+    total = 0
+    col = 1
+    row = 2
+
+    all_cols = availability_container.find_elements_by_xpath(".//*")
+
+    # TODO this is hard coded, maybe try to find a better way
+    num_cols  = 7
+
+    #vvvv all time slots + headers all_cols/2 - number of headers
+    num_rows = (int(len(all_cols))/2 - num_cols) / num_cols
+
+    for col in all_cols:
+        while row <= num_rows:
+            try: # Check the background color of each appt, if green add to total
+  
+                cur_time_slot = col.find_element_by_xpath('.//div[{0}]/div'.format(row))
+                button_color = Color.from_string(cur_time_slot.value_of_css_property('background-color'))
+                if button_color != GREY_COLOR:
+                    total+=1
+            except:
+                row+=1
+                continue
+            row+=1
+    #print(total)
+    return total
+
+
 
 key_file = 'keys.json'
 with open(key_file) as f:
@@ -23,7 +57,6 @@ chrome_options = Options()
 #chrome_options.add_argument('--headless')
 driver = webdriver.Chrome(options=chrome_options,executable_path='Drivers/chromedriver')
 
-
 while True:
 
     try:
@@ -32,7 +65,7 @@ while True:
         time.sleep(random.uniform(1.4,2.3))
     except:
         time.sleep(random.uniform(120,150))
-        print("URL Did not load.")
+        #print("URL Did not load.")
         continue
 
 
@@ -44,7 +77,7 @@ while True:
         )
     except:
         time.sleep(random.uniform(3.2,4.6))
-        print("No root tag.")
+        #print("No root tag.")
         continue
 
     
@@ -55,13 +88,12 @@ while True:
         )
         
     except: # Portal not open, no appointmetns
-        print("portal not open")
+        #print("portal not open")
         continue
 
 
     try: # Portal is open, click the Get Started button on the introduction page
-        print("trying to click button")
-        # TODO click continue button here
+        #print("trying to click button")
         get_started_button = WebDriverWait(driver, 2).until(
             EC.presence_of_element_located((By.XPATH, '/html/body/curo-root/curo-intel-widget-layout/div/curo-registrations/curo-registration-steps/div/div/div/div[2]/curo-patient-registration/div[2]/button'))
         )
@@ -69,26 +101,48 @@ while True:
 
     
     except: # Could not click continue button
-        print("couldnt click button")
+        #print("couldnt click button")
+        continue
+
+
+    try: # Click first location
+        #print("trying to click location")
+        
+        loc_1 = WebDriverWait(driver, 2).until(
+            EC.presence_of_element_located((By.XPATH, '/html/body/curo-root/curo-intel-widget-layout/div/curo-registrations/curo-registration-steps/div/div/div/div[2]/curo-patient-registration/curo-locations/div/div[2]/div[3]/div[1]/div[1]'))
+        ).text
+        #print(loc_1)
+        
+        location_1_button = WebDriverWait(driver, 2).until(
+            EC.presence_of_element_located((By.XPATH, '/html/body/curo-root/curo-intel-widget-layout/div/curo-registrations/curo-registration-steps/div/div/div/div[2]/curo-patient-registration/curo-locations/div/div[2]/div[3]'))
+        )
+        location_1_button.click()
+        time.sleep(random.uniform(1.4,2.3))
+
+    
+    except: # Could not click continue button
+        #print("couldnt click location")
         continue
 
 
     try: # Count appointments for first location
-        print("click button worked")
+        #print("click button worked")
         openAppts = 0
         loc1_availability_container = WebDriverWait(driver, 2).until(
             EC.presence_of_element_located((By.XPATH, '/html/body/curo-root/curo-intel-widget-layout/div/curo-registrations/curo-registration-steps/div/div/div/div[2]/curo-patient-registration/curo-availabilities/section/div[2]'))
         )
-        openAppts += countAppointments(availability_container)
-        print("made it through function")
+        openAppts += countAppointments(loc1_availability_container)
+        #print("line 128")
         if openAppts > 0:
-            playsound('Beep.m4a')
+            print("i found open appts:")
+            print(openAppts)
+            
             driver.get_screenshot_as_file("Screenshots/VNACJcapture.png")
 
             status = "VNACJ: Portal is open at this link https://curogram.com/registrations/5fe2fe643b4a850044b0b3b1"
             imagePath = "Screenshots/VNACJcapture.png"
 
-            print(element.get_attribute("class"))
+            playsound('Beep.m4a')
 
             # TODO uncomment once we have it fully working
             # api.update_with_media(imagePath, status)
@@ -100,54 +154,3 @@ while True:
 
 
     # TODO count appointments for the other 2 locations too
-
-
-
-
-
-def countAppointments(availability_container):
-    # TODO get the correct green color vvvvvvv and update this placeholder
-    GREEN_COLOR = Color.from_string('#2F7ED8')
-    total = 0
-    col = 1
-    row = 2
-
-    print("in countAppts function")
-
-    num_cols = availability_container.childElementCount
-
-    col1 = availability_container.find_element_by_xpath('.//div[1]')
-    #col1 = WebDriverWait(driver, 2).until(
-    #        EC.presence_of_element_located((By.XPATH, '/html/body/curo-root/curo-intel-widget-layout/div/curo-registrations/curo-registration-steps/div/div/div/div[2]/curo-patient-registration/curo-availabilities/section/div[2]/div[1]'))
-    #    )
-
-    num_rows = col1.childElementCount
-  
-
-    while col <= num_cols:
-        while row <= num_rows:
-            try: # Check the background color of each appt, if green add to total
-                cur_time_slot = availability_container.find_element_by_xpath('.//div[{0}]/div[{1}]/div'.format(num_col, num_row))
-                #cur_time_slot = WebDriverWait(driver, 2).until(
-                #                EC.presence_of_element_located((By.XPATH, '/html/body/curo-root/curo-intel-widget-layout/div/curo-registrations/curo-registration-steps/div/div/div/div[2]/curo-patient-registration/curo-availabilities/section/div[2]/div[{0}]/div[{1}]/div'.format(num_col, num_row)))
-                #            )
-                button_color = Color.from_string(cur_time_slot.value_of_css_property('background-color'))
-  
-                if button_color == GREEN_COLOR:
-                    total+=1
-            except:
-                continue
-            row+=1
-        col+=1
-
-    return total
-
-
-
-
-
-
-
-
-
-
