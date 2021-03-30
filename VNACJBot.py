f05fc8e4469321834cb79b160d1cc58267ff3e20
Gianna Aprile <giannaaprile99@gmail.com>
2021-03-30 19:45:25 -0400

    VNACJ finished

--- VNACJBot.py
+++ VNACJBot.py
@@ -115,10 +115,9 @@ while True:
     try: # Click first location
         #print("trying to click location")
         
-        loc_1 = WebDriverWait(driver, 2).until(
+        loc1_name = WebDriverWait(driver, 2).until(
             EC.presence_of_element_located((By.XPATH, '/html/body/curo-root/curo-intel-widget-layout/div/curo-registrations/curo-registration-steps/div/div/div/div[2]/curo-patient-registration/curo-locations/div/div[2]/div[3]/div[1]/div[1]'))
         ).text
-        #print(loc_1)
         
         location_1_button = WebDriverWait(driver, 2).until(
             EC.presence_of_element_located((By.XPATH, '/html/body/curo-root/curo-intel-widget-layout/div/curo-registrations/curo-registration-steps/div/div/div/div[2]/curo-patient-registration/curo-locations/div/div[2]/div[3]'))
@@ -133,32 +132,103 @@ while True:
 
 
     try: # Count appointments for first location
-        #print("click button worked")
-        openAppts = 0
+        screenshot_taken = False
+        loc1_openAppts = 0
         loc1_availability_container = WebDriverWait(driver, 2).until(
             EC.presence_of_element_located((By.XPATH, '/html/body/curo-root/curo-intel-widget-layout/div/curo-registrations/curo-registration-steps/div/div/div/div[2]/curo-patient-registration/curo-availabilities/section/div[2]'))
         )
-        openAppts += countAppointments(loc1_availability_container)
-        #print("line 128")
-        if openAppts > 0:
-            print("i found")
-            print(openAppts)
-            print("open appts at:")
-            print(loc_1)
-            
-            
+        loc1_openAppts += countAppointments(loc1_availability_container)
+        
+        if loc1_openAppts > 0:
             driver.get_screenshot_as_file("Screenshots/VNACJcapture.png")
+            screenshot_taken = True
 
-            status = "VNACJ: Portal is open at this link https://curogram.com/registrations/5fe2fe643b4a850044b0b3b1"
-            imagePath = "Screenshots/VNACJcapture.png"
-
-            api.update_with_media(imagePath, status)
-            print("Appointment(s) found")
-            break
         
     except:
         continue
 
 
 
-    # TODO count appointments for the other 2 locations too
+    try: # Count appointments for second location
+        loc2_openAppts = 0
+
+        dropdown_menu = WebDriverWait(driver, 2).until(
+            EC.presence_of_element_located((By.XPATH, '/html/body/curo-root/curo-intel-widget-layout/div/curo-registrations/curo-registration-steps/div/div/div/div[2]/curo-patient-registration/div[2]/ng-select'))
+        )
+        dropdown_menu.click()
+
+        loc2_dropdown = dropdown_menu.find_element_by_xpath('/html/body/curo-root/curo-intel-widget-layout/div/curo-registrations/curo-registration-steps/div/div/div/div[2]/curo-patient-registration/div[2]/ng-select/ng-dropdown-panel/div/div[2]/div[2]')
+        loc2_dropdown.click()
+
+        time.sleep(random.uniform(0.3,0.7))
+        
+        loc2_name = WebDriverWait(driver, 2).until(
+            EC.presence_of_element_located((By.XPATH, '/html/body/curo-root/curo-intel-widget-layout/div/curo-registrations/curo-registration-steps/div/div/div/div[2]/curo-patient-registration/div[2]/ng-select/ng-dropdown-panel/div/div[2]/div[2]/span')).text
+        )
+
+        loc2_availability_container = WebDriverWait(driver, 2).until(
+            EC.presence_of_element_located((By.XPATH, '/html/body/curo-root/curo-intel-widget-layout/div/curo-registrations/curo-registration-steps/div/div/div/div[2]/curo-patient-registration/curo-availabilities/section/div[2]'))
+        )
+        loc2_openAppts += countAppointments(loc2_availability_container)
+
+        if loc2_openAppts > 0 and screenshot_taken == False:
+            driver.get_screenshot_as_file("Screenshots/VNACJcapture.png")
+            screenshot_taken = True
+
+        
+    except:
+        continue
+
+
+    try: # Count appointments for third location
+        loc3_openAppts = 0
+
+        dropdown_menu = WebDriverWait(driver, 2).until(
+            EC.presence_of_element_located((By.XPATH, '/html/body/curo-root/curo-intel-widget-layout/div/curo-registrations/curo-registration-steps/div/div/div/div[2]/curo-patient-registration/div[2]/ng-select'))
+        )
+        dropdown_menu.click()
+
+        loc3_dropdown = dropdown_menu.find_element_by_xpath('/html/body/curo-root/curo-intel-widget-layout/div/curo-registrations/curo-registration-steps/div/div/div/div[2]/curo-patient-registration/div[2]/ng-select/ng-dropdown-panel/div/div[2]/div[1]')
+        loc3_dropdown.click()
+        
+        time.sleep(random.uniform(0.3,0.7))
+
+        loc3_name = WebDriverWait(driver, 2).until(
+            EC.presence_of_element_located((By.XPATH, '/html/body/curo-root/curo-intel-widget-layout/div/curo-registrations/curo-registration-steps/div/div/div/div[2]/curo-patient-registration/div[2]/ng-select/ng-dropdown-panel/div/div[2]/div[1]/span')).text
+        )
+
+        loc3_availability_container = WebDriverWait(driver, 2).until(
+            EC.presence_of_element_located((By.XPATH, '/html/body/curo-root/curo-intel-widget-layout/div/curo-registrations/curo-registration-steps/div/div/div/div[2]/curo-patient-registration/curo-availabilities/section/div[2]'))
+        )
+        loc3_openAppts += countAppointments(loc3_availability_container)
+
+        if loc3_openAppts > 0 and screenshot_taken == False:
+            driver.get_screenshot_as_file("Screenshots/VNACJcapture.png")
+            screenshot_taken = True
+ 
+    except:
+        continue
+
+
+    try:
+        if screenshot_taken:
+            appts_tweet = ''
+            total_appts = loc1_openAppts + loc2_openAppts + loc3_openAppts
+            
+            if loc1_openAppts > 0:
+                appts_tweet = appts_tweet + ('\n{0}: {1}'.format(loc1_name,loc1_openAppts))
+            if loc2_openAppts > 0:
+                appts_tweet = appts_tweet + ('\n{0}: {1}'.format(loc2_name,loc2_openAppts))
+            if loc3_openAppts > 0:
+                appts_tweet = appts_tweet + ('\n{0}: {1}'.format(loc3_name,loc3_openAppts))
+
+            intro = "VNACJ: {0} appointments open at this link https://curogram.com/registrations/5fe2fe643b4a850044b0b3b1 \n".format(total_appts)
+            status = intro + appts_tweet
+            imagePath = "Screenshots/VNACJcapture.png"
+
+            api.update_with_media(imagePath, status)
+            print("Appointment(s) found")
+            break
+
+    except:
+        continue
