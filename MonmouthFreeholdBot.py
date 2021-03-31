1770721c7347eaf5cda0f77bdccff023a3a8acdc
Kyle Litton <kylelitton@Kyles-iMac.local>
2021-03-31 16:37:22 -0400

    Small changes to freehold bot

--- MonmouthFreeholdBot.py
+++ MonmouthFreeholdBot.py
@@ -3,7 +3,6 @@ from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
-from playsound import playsound
 import tweepy
 import random
 import time
